b9a2d9e7596f260048673b5f74514c27d980ddc5
Jens Jahnke <jan0sch@gmx.net>
2013-04-16 15:00:46 +0200

    Saubere Typo3-Installation deaktiviert.

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,17 +11,17 @@ apt-get install -y apache2 mysql-server-5.5 libapache2-mod-php5 php5-curl php5-g
 wget http://get.typo3.org/blank -O "${TYPO3_SRC}"
 tar zxf "${TYPO3_SRC}" --strip-components 1 -C "${TYPO3_WWW}"
 
-mkdir -p "${TYPO3_DIR}"
-mv "${TYPO3_WWW}/typo3_src/*" "${TYPO3_DIR}/"
-rm -f "${TYPO3_WWW}/index.php"
-rm -f "${TYPO3_WWW}/t3lib"
-rm -f "${TYPO3_WWW}/typo3"
-rm -rf "${TYPO3_WWW}/typo3_src*"
-
-ln -s "${TYPO3_DIR}" /var/www/typo3_src
-ln -s /var/www/typo3_src/t3lib /var/www/t3lib
-ln -s /var/www/typo3_src/typo3 /var/www/typo3
-ln -s /var/www/typo3_src/index.php /var/www/index.php
+#mkdir -p "${TYPO3_DIR}"
+#mv "${TYPO3_WWW}/typo3_src/*" "${TYPO3_DIR}/"
+#rm -f "${TYPO3_WWW}/index.php"
+#rm -f "${TYPO3_WWW}/t3lib"
+#rm -f "${TYPO3_WWW}/typo3"
+#rm -rf "${TYPO3_WWW}/typo3_src*"
+#
+#ln -s "${TYPO3_DIR}" /var/www/typo3_src
+#ln -s /var/www/typo3_src/t3lib /var/www/t3lib
+#ln -s /var/www/typo3_src/typo3 /var/www/typo3
+#ln -s /var/www/typo3_src/index.php /var/www/index.php
 
 chown -Rfh www-data:www-data "${TYPO3_WWW}/fileadmin" "${TYPO3_WWW}/typo3temp" "${TYPO3_WWW}/typo3conf" "${TYPO3_WWW}/uploads"
 
