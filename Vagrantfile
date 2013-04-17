b97229265a4bc716bc0e9055637297242ef3764d
Jens Jahnke <jan0sch@gmx.net>
2013-04-17 11:58:16 +0200

    Bootstrapping nur noch für Typo3.

--- Vagrantfile
+++ Vagrantfile
@@ -5,7 +5,6 @@ Vagrant.configure("2") do |config|
   config.vm.box = "precise64"
   config.vm.box_url = "http://files.vagrantup.com/precise64.box"
   config.vm.network :forwarded_port, guest: 80, host: 8080
-  #config.vm.provision :shell, :path => "bootstrap.sh"
   config.vm.synced_folder "src/fileadmin", "/var/www/fileadmin"
   config.vm.provision :chef_solo do |chef|
     chef.json = {
@@ -27,4 +26,5 @@ Vagrant.configure("2") do |config|
     chef.add_recipe "php"
     chef.add_recipe "vim"
   end
+  config.vm.provision :shell, :path => "bootstrap-typo3.sh"
 end
--- bootstrap-typo3.sh
+++ bootstrap-typo3.sh
@@ -4,9 +4,6 @@ TYPO3_SRC=typo3.tar.gz
 TYPO3_DIR=/usr/local/share/typo3
 TYPO3_WWW=/var/www
 
-export DEBIAN_FRONTEND=noninteractive
-apt-get update
-apt-get install -y apache2 mysql-server-5.5 libapache2-mod-php5 php5-curl php5-gd php5-imagick php5-mysql
 rm -f "${TYPO3_WWW}/index.html"
 
 if [[ ! -f "${TYPO3_WWW}/index.php" ]]; then
@@ -15,6 +12,3 @@ if [[ ! -f "${TYPO3_WWW}/index.php" ]]; then
   touch "${TYPO3_WWW}/typo3conf/ENABLE_INSTALL_TOOL"
   chown -Rfh www-data:www-data "${TYPO3_WWW}/fileadmin" "${TYPO3_WWW}/typo3temp" "${TYPO3_WWW}/typo3conf" "${TYPO3_WWW}/uploads"
 fi
-
-service apache2 restart
-service mysql restart
