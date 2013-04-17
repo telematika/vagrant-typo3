2bae0b040cd173648f4420a488d2f29c5a7b1a6f
Jens Jahnke <jan0sch@gmx.net>
2013-04-17 12:05:07 +0200

    Statusausgaben des Shellskripts.

--- bootstrap-typo3.sh
+++ bootstrap-typo3.sh
@@ -7,8 +7,13 @@ TYPO3_WWW=/var/www
 rm -f "${TYPO3_WWW}/index.html"
 
 if [[ ! -f "${TYPO3_WWW}/index.php" ]]; then
+  echo "Downloading typo3..."
   wget --quiet http://get.typo3.org/blank -O "${TYPO3_SRC}"
+  echo "Extracting typo3..."
   tar zxf "${TYPO3_SRC}" --strip-components 1 -C "${TYPO3_WWW}"
+  echo "Enabling INSTALL TOOL..."
   touch "${TYPO3_WWW}/typo3conf/ENABLE_INSTALL_TOOL"
+  echo "Setting file system permissions..."
   chown -Rfh www-data:www-data "${TYPO3_WWW}/fileadmin" "${TYPO3_WWW}/typo3temp" "${TYPO3_WWW}/typo3conf" "${TYPO3_WWW}/uploads"
+  echo "Done."
 fi
